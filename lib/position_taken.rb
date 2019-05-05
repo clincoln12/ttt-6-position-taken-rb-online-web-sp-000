3ff37afe1d14f793c2622ade21ac0a4cbb904455
Chris Lincoln <chris.lincoln12@gmail.com>
2019-05-05 12:55:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -10,7 +10,7 @@ def position_taken?(board, index)
       elsif cell == nil
        return false
        
-     else cell == "X" || "O"
-       return true
+       else cell == "X" || "O"
+        return true
  end
 end
